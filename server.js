9d1c4b54099b09b35d42a0a8e89751565fd3c0f0
Braingames006 <braingames506@gmail.com>
2025-05-27 22:30:31 +0530

    fix: simplify CORS configuration in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
